50b70a6eedada35f5714b56a6c41d20024332b3c
karolien <karolien@hotmail.co.nz>
2018-05-12 17:48:30 +1200

    Add pastlife webapp

---
+++ pastlife/main.js
@@ -0,0 +1,26 @@
+clearPreviousPastLife();
+
+function getPastLife(){
+    clearPreviousPastLife();
+    var uri = "http://uinames.com/api/";
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", uri, true);
+    xhr.onload = function () {
+        var pastlifeData = JSON.parse(xhr.responseText);
+        document.getElementById("year").innerHTML = "Born: " + Math.round(Math.random() * (2018-1000)+1000);
+        document.getElementById("name").innerHTML = "Name: " + pastlifeData.name + " " + pastlifeData.surname;
+        document.getElementById("country").innerHTML = "Country: " + pastlifeData.region;
+        
+    }
+    xhr.send(null);
+}
+
+function getPunchline(){
+    document.getElementById("punchline").innerHTML = jokePunchline;
+}
+
+function clearPreviousPastLife(){
+    document.getElementById("year").innerHTML = "";
+    document.getElementById("name").innerHTML = "";
+    document.getElementById("country").innerHTML = "";
+}
